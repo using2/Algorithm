43686c9cac3399a179ecf8ae13f2159a825a42a7
강유진 <136972840+using2@users.noreply.github.com>
2024-10-15 00:25:19 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.16 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -0,0 +1,7 @@
+function solution(myString, pat) {
+    var answer = 0;
+    myString = myString.toLowerCase();
+    pat = pat.toLowerCase();
+    answer = myString.includes(pat)? 1 : 0;
+    return answer;
+}
