969a8ced0370a2625892e9199d028bf20c25b8d0
강유진 <136972840+using2@users.noreply.github.com>
2025-07-19 22:56:17 +0900

    [level 3] Title: 네트워크, Time: 0.36 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -0,0 +1,23 @@
+function solution(n, computers) {
+  const visited = Array(n).fill(false);
+  let count = 0;
+
+  function dfs(node) {
+    visited[node] = true;
+
+    for (let i = 0; i < n; i++) {
+      if (!visited[i] && computers[node][i] === 1) {
+        dfs(i);
+      }
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (!visited[i]) {
+      dfs(i);
+      count++;
+    }
+  }
+
+  return count;
+}
